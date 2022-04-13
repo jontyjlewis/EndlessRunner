238719c12540596d59395e3610738147a2c1f62d
Jonathan Lewis <jontyjlewis@gmail.com>
2022-04-13 15:51:01 -0700

    centered canvas

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 // console.log("Hello from main.js");
 
 let config = {
-    type: Phaser.CANVAS,
-    width: 500,
-    height: 700,
+    type: Phaser.AUTO,
     scene: [Menu],
-}
+    scale: {
+        width: 500,
+        height: 700,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        mode: Phaser.Scale.FIT,
+    }
+};
 
 let game = new Phaser.Game(config);
